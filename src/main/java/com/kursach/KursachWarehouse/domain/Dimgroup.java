75985762439e125e9dcc14c7393023d539e1314e
DaniilKorshov <36723384+DaniilKorshov@users.noreply.github.com>
2019-04-30 13:43:12 +0300

    add entities

---
+++ src/main/java/com/kursach/KursachWarehouse/domain/Dimgroup.java
@@ -0,0 +1,59 @@
+package com.kursach.KursachWarehouse.domain;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name="dimgroup")
+public class Dimgroup {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private double width;
+    private double length;
+
+    private double weight;
+
+    private String color;
+    private String config;
+
+    public Dimgroup() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getConfig() {
+        return config;
+    }
+    public void setConfig(String config) {
+        this.config = config;
+    }
+}
---
+++ src/main/java/com/kursach/KursachWarehouse/domain/enums/CellStatus.java
@@ -0,0 +1,5 @@
+package com.kursach.KursachWarehouse.domain.enums;
+
+public enum CellStatus {
+    EMPTY,TAKEN,RESERVED;
+}
